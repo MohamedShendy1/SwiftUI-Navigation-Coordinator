ab83c89b73538ea1eb54547f1ec96b323163e6cd
Mohamed Shendy <dev.mohamed.shendy@gmail.com>
2026-03-03 01:17:43 +0200

    Initial Commit

---
+++ SwiftUI-Navigation-Coordinator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Navigation-Coordinator
+//
+//  Created by mac on 03/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Navigation-Coordinator/SwiftUI_Navigation_CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Navigation_CoordinatorApp.swift
+//  SwiftUI-Navigation-Coordinator
+//
+//  Created by mac on 03/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Navigation_CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
